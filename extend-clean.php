39603ae557b4445a0b5e2bf46db46db065f16ec9
William P. Davis <will.davis@gmail.com>
2012-05-10 16:37:03 +0300

    0.4b

--- extend-clean.php
+++ extend-clean.php
@@ -1,9 +1,42 @@
 <?php
 /*
 Plugin Name: Docs to WordPress extender - clean content, strip comments
+Author: William P. Davis, Bangor Daily News
+Author URI: http://wpdavis.com/
+Version: 0.4-beta
 */
 
-	
+
+add_filter( 'pre_docs_to_wp_strip', 'dtwp_extract_styles', 10, 1 );
+function dtwp_extract_styles( $contents ) {
+
+		//PHP doesn't honor lazy matches very well, apparently, so add newlines
+		$contents[ 'contents' ] = str_replace( '}', "}\r\n", $contents[ 'contents' ] );
+
+
+		preg_match_all( '#.c(?P<digit>\d+){(.*?)font-weight:bold(.*?)}#', $contents[ 'contents' ], $boldmatches );
+		preg_match_all('#.c(?P<digit>\d+){(.*?)font-style:italic(.*?)}#', $contents[ 'contents' ], $italicmatches);
+		
+		if( !empty( $boldmatches[ 'digit' ] ) ) {
+		
+			foreach( $boldmatches[ 'digit' ] as $boldclass ) {
+				$contents[ 'contents' ] = preg_replace( '#<span class="(.*?)c' . $boldclass . '(.*?)">(.*?)</span>#s', '<span class="$1c' . $boldclass . '$2"><strong>$3</strong></span>', $contents[ 'contents' ] );
+			}
+		
+		}
+		
+		if( !empty( $italicmatches[ 'digit' ] ) ) {
+		
+			foreach( $italicmatches[ 'digit' ] as $italicclass ) {
+				$contents[ 'contents' ] = preg_replace( '#<span class="(.*?)c' . $italicclass . '(.*?)">(.*?)</span>#s', '<span class="$1c' . $italicclass . '$2"><em>$3</em>', $contents[ 'contents' ] );
+			}
+		
+		}
+		
+		return $contents;
+
+}
+
 
 add_filter( 'pre_docs_to_wp_insert', 'dtwp_clean_content_filter', 10, 1 );
 function dtwp_clean_content_filter( $post_array ) {	
@@ -32,7 +65,7 @@ function dtwp_clean_content($post_content) {
 		
 		$post_content = str_replace( '<div>','<p>',$post_content );
 		$post_content = str_replace( '</div>', '</p>',$post_content );
-		$post_content = strip_tags($post_content, '<strong><b><i><em><a><u><br><p><ol><ul><li>' );
+		$post_content = strip_tags($post_content, '<strong><b><i><em><a><u><br><p><ol><ul><li><h1><h2><h3><h4><h5><h6>' );
 		$post_content = str_replace( '--','&mdash;',$post_content );
 		$post_content = str_replace( '<br><br>','<p>',$post_content );
 		$post_content = str_replace( '<br>&nbsp;&nbsp;&nbsp;', '\n\n', $post_content );
